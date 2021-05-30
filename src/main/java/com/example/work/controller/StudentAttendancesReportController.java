b95de8e187c2ebc7c0937a11615d2c4280b1f242
Edhar <avuzia@gmail.com>
2021-05-31 00:12:07 +0300

    Fuck this fuck!!!

--- src/main/java/com/example/work/controller/StudentAttendancesReportController.java
+++ src/main/java/com/example/work/controller/StudentAttendancesReportController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-пойми, почему не работает для для других групп, курсов
 @Setter
 @RestController
 @RequestMapping("api/student-attendances-report")
